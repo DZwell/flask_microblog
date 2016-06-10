9d8261e9eb19fdcab667e57393f6eeecba912c4d
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-06-10 12:27:01 -0700

    Add form validators with flash messages

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -18,3 +19,13 @@ def index():
 
     ]
     return render_template('index.html', title=None, user=user, posts=posts)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    """Return login form."""
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID={}, remember_me={}'.format(form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
