3893aa519828b2c589cf3ce78e314f055b315b47
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-06-10 11:36:56 -0700

    Add WTForms config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object('config')
 # Import statement here is avoid circular import. We import app in views later.
 from app import views
---
+++ config.py
@@ -0,0 +1,3 @@
+import os
+WTF_CSRF_ENABLED = True
+SECRET_KEY = os.environ.get('SECRET_KEY')
