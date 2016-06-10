5371f049c278b1fbad5e3bd87a9298b1b2c6f481
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-06-10 11:02:11 -0700

    Add fake authors/posts

--- app/views.py
+++ app/views.py
@@ -1,7 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    """Return hello world."""
-    return 'Hello world.'
+    """Return index."""
+    user = {'name': 'DZ'}
+    posts = [
+        {
+            'author': {'name': 'Bert'},
+            'body': 'My what a beautiful day today.'
+        },
+        {
+            'author': {'name': 'Bartran'},
+            'body': 'Indded it is a beautiful day today.'
+        }
+
+    ]
+    return render_template('index.html', title=None, user=user, posts=posts)
